82410579aa89bf9a8efaadac3411788aab183e56
woookkk <dndnrudrud@naver.com>
2020-10-16 09:43:12 +0900

    미디어쿼리

--- js/franchisee.js
+++ js/franchisee.js
@@ -1,7 +1,138 @@
-$(function(){////////////// jQB /////////////////////
+$(function () { ////////////// jQB /////////////////////
     console.log("로딩완료!");
-    
-    
+
+    //부드러운 스크롤 
+    //$(".franchisee_wrap").smoothWheel();
+
+    /* 스크롤 */
+    AOS.init();
+
+
+    /* 패럴렉스 */
+    $(".sec1_img_back").enllax({
+
+    });
+
+    // 1.현재 페이지번호
+    var pno = 0;
+
+    // 2.스크롤액션 기준위치
+    var p1pos, p2pos, p3pos, p4pos;
+    // 3.기준선 범위 갭
+    var gap = 200;
+    // 4.메뉴를 클릭할 경우 상태변수
+    var click_sts = 0; //0-클릭안함,1-클릭함
+
+
+    $(window).scroll(function (e) {
+
+        // 탑버튼위치값(top:70%)
+        var tbpos = $(window).height() * 0.8;
+        //console.log("70%값:" + tbpos);
+
+        // 현재 스크롤 위치값(세로스크롤)
+        var scTop = $(this).scrollTop();
+        //console.log("위치:" + scTop);
+
+        // 1.스크롤위치가 200px 미만일때
+        if (scTop < 200) {
+
+            // 버튼숨기고 처음위치로
+            $(".topbtn").hide().css({
+                top: tbpos + "px"
+            }); /// css ///////
+
+        } //// if ////////////////////
+        // 2.스크롤위치가 200px 이상일때
+        else {
+
+            // 버튼보이고 위치이동하기
+            $(".topbtn").show().stop()
+                .animate({
+                    top: (tbpos + scTop) + "px"
+                }, 400, "easeOutCirc");
+
+        } //// else //////////////////
+
+
+
+    }); //////////////// scroll //////////////////////////////////////////////
+
+
+
+
+    /// 스크롤액션 기준요소 top값 셋팅
+    // offset().top 으로 각 요소의 top값 읽어오기
+    p1pos = $("#page1").offset().top;
+    p2pos = $("#page2").offset().top;
+    p3pos = $("#page3").offset().top;
+    p4pos = $("#page4").offset().top;
+
+    console.log("위치:" + p1pos + "/" + p2pos + "/" + p3pos + "/" + p4pos);
+
+
+
+
+
+    /// 페이지 새로 고칠때 맨위로 가게하기(브라우저 캐쉬가 잘 안지워져서 쓰는 것임!) 
+    setTimeout(function () {
+        $("html,body").animate({
+            scrollTop: "0px"
+        }, 10); /// animate ////
+    }, 500); //// 타임아웃 ////////
+
+
+
+
+    // GNB a링크를 클릭하면 해당 페이지위치로 이동 애니메이션
+    // 이벤트 대상 : .franchisee_menu_list a
+    $(".franchisee_menu_list a").click(function (e) {
+        e.preventDefault();
+
+        // 스크롤 이벤트막기 상태값 변경!
+        click_sts = 1;
+
+        // 클릭된 a요소의 부모li순번을 pno에 넣기!
+        var idx = $(this).parent().index();
+        pno = idx;
+        console.log("메뉴순번:" + idx);
+
+        var pid = $(this).attr("href");
+
+        // 2. 이동할 위치의 아이디 top값을 구함
+        var pgpos = $(pid).offset().top;
+
+        // 3. 스크롤에 애니메이션을 설정하여 해당위치로 이동
+        $("html,body").stop().animate({
+            scrollTop: pgpos + "px"
+        }, 800, "easeInOutQuint", function () {
+            //이동후 클릭상태값 해제하기
+            click_sts = 0;
+        }); /// animate //////
+
+
+    }); //////// click /////////////
+    ////////////////////////////////
+
+    /// 탑버튼 클릭시 맨위로 이동하기 ////
+    $(".topbtn").click(function () {
+        click_sts = 1; //스크롤잠금
+        $("html,body").animate({
+            scrollTop: "0px"
+        }, 800, "easeInOutQuint", function () {
+            click_sts = 0; //해제   
+        }); /// animate /////
+
+    }); ////////// click ///////////
+
+
+
+
+
+
+
+
+
     var stepT = [
         "가맹점 문의상담",
         "입지조사 및 상권분석",
@@ -12,7 +143,7 @@ $(function(){////////////// jQB /////////////////////
         "개업준비",
         "오픈 OPEN"
     ]
-    
+
     var stepLf = [
         "- 홈페이지 접수 및 전화",
         "- 사업타당성 검토(정보제공)",
@@ -43,61 +174,59 @@ $(function(){////////////// jQB /////////////////////
         "- 오픈판촉 지도",
         "&nbsp;"
     ]
-    
-    
-    
-    
+
+
+
+
     /// 1. 메뉴박스에 html생성 및 초기화, 캡션글 넣기
-        // 대상 : .step_list_wrap
-    for (var i = 0; i < 4; i++){
+    // 대상 : .step_list_wrap
+    for (var i = 0; i < 4; i++) {
         $(".step_list_wrap1").append(
-        '<ul class="step_list_box">'+
-        '<li class="step_list_img">'+
-        '<div class="step_img">'+
-        '<img src="images/step'+(i+1)+'.png" alt="개설절차">'+
-        '<div class="step_list_num">STEP'+(i+1)+'</div>'+
-        '</div>'+
-        '</li>'+
-        '<li class="step_list_text">'+
-        '<ul>'+
-        '<li class="step_text_title">'+stepT[i]+'</li>'+
-        '<li>'+stepLf[i]+'</li>'+
-        '<li>'+stepLs[i]+'</li>'+
-        '<li>'+stepLt[i]+'</li>'+
-        '</ul>'+
-        '</li>'+
-        '</ul>'
-            
+            '<ul class="step_list_box">' +
+            '<li class="step_list_img">' +
+            '<div class="step_img">' +
+            '<img src="images/step' + (i + 1) + '.png" alt="개설절차">' +
+            '<div class="step_list_num">STEP' + (i + 1) + '</div>' +
+            '</div>' +
+            '</li>' +
+            '<li class="step_list_text">' +
+            '<ul>' +
+            '<li class="step_text_title">' + stepT[i] + '</li>' +
+            '<li>' + stepLf[i] + '</li>' +
+            '<li>' + stepLs[i] + '</li>' +
+            '<li>' + stepLt[i] + '</li>' +
+            '</ul>' +
+            '</li>' +
+            '</ul>'
+
         );
-          
-    }//////////// for ////////////
-  
-    for (var i = 4; i < 8; i++){
+
+    } //////////// for ////////////
+
+    for (var i = 4; i < 8; i++) {
         $(".step_list_wrap2").append(
-        '<ul class="step_list_box">'+
-        '<li class="step_list_img">'+
-        '<div class="step_img">'+
-        '<img src="images/step'+(i+1)+'.png" alt="개설절차">'+
-        '<div class="step_list_num">STEP'+(i+1)+'</div>'+
-        '</div>'+
-        '</li>'+
-        '<li class="step_list_text">'+
-        '<ul>'+
-        '<li class="step_text_title">'+stepT[i]+'</li>'+
-        '<li>'+stepLf[i]+'</li>'+
-        '<li>'+stepLs[i]+'</li>'+
-        '<li>'+stepLt[i]+'</li>'+
-        '</ul>'+
-        '</li>'+
-        '</ul>'
-            
+            '<ul class="step_list_box">' +
+            '<li class="step_list_img">' +
+            '<div class="step_img">' +
+            '<img src="images/step' + (i + 1) + '.png" alt="개설절차">' +
+            '<div class="step_list_num">STEP' + (i + 1) + '</div>' +
+            '</div>' +
+            '</li>' +
+            '<li class="step_list_text">' +
+            '<ul>' +
+            '<li class="step_text_title">' + stepT[i] + '</li>' +
+            '<li>' + stepLf[i] + '</li>' +
+            '<li>' + stepLs[i] + '</li>' +
+            '<li>' + stepLt[i] + '</li>' +
+            '</ul>' +
+            '</li>' +
+            '</ul>'
+
         );
-          
-    }//////////// for ////////////
-  
-    
-    
-});//////////////////// jQB /////////////////////////
+
+    } //////////// for ////////////
+
+
+
+}); //////////////////// jQB /////////////////////////
 /////////////////////////////////////////////////////
-    
-   
--- js/info.js
+++ js/info.js
@@ -1,3 +1,57 @@
-$(function(){//////// jQB ////////
-    
-});/////////////// jQB ///////////////
+$(function () { ///////// jQB ///////////
+    //console.log("로딩완료");
+
+
+    var ftop = $(".info_icon_box").offset().top;
+    //////// 스크롤 이벤트 설정하기 //////////
+    $(window).scroll(function () {
+        //현재 window의 스크롤바 위치(세로 스크롤)
+        var cTop = $(this).scrollTop();
+        console.log("현재스크롤바위치: " + cTop);
+        console.log(ftop);
+
+        if (cTop > ftop - 800 && cTop < ftop - 300) {
+            $(".info_icon_list").addClass("active");
+        } else {
+
+            $(".info_icon_list").removeClass("active");
+        }
+
+    }); ////////////// scroll ////////////////
+
+
+
+
+
+    /********** 타이핑 효과 ********/
+
+    var typed6 = new Typed('#typed6', {
+        strings: ['고객의 욕구에 부합하는 제품과 서비스를 제공함으로써^500\n고객에게 신뢰와 사랑을 받는 서도기업으로 도약하겠습니다.'
+
+    ],
+        // 타이핑스피드
+        typeSpeed: 50,
+        // 지우기스피드
+        backSpeed: 0,
+        // 반복여부
+        loop: true
+    });
+
+
+}); ///////////////// jQB /////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
